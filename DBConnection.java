3eafa9c5455038816d6ed354c937ac2f9b6dff30
Pallavi Suryawanshi <pallavisuryawanshi225@gmail.com>
2026-01-22 01:16:37 +0530

    Add files via upload

---
+++ DBConnection.java
@@ -0,0 +1,21 @@
+package numberguessinggame;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/number_game";
+    private static final String USER = "root";
+    private static final String PASS = "pallu@15";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
